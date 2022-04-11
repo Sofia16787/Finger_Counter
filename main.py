ea1bd2c3a3b5151e6bc2413dfe24d376f87f8060
Sofia16787 <102724961+Sofia16787@users.noreply.github.com>
2022-04-11 16:24:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2 
 import mediapipe as mp 
+import time  
 
 cap = cv2.VideoCapture(0) # подключение к камере
 mp_Hands = mp.solutions.hands # хотим распозновать руки (hands)
@@ -7,10 +8,11 @@ hands = mp_Hands.Hands(max_num_hands = 2) # характеристики для 
 mpDraw = mp.solutions.drawing_utils # иннициализация утилит для рисования
 
 finger_Coord = [(8, 6), (12, 10), (16, 14), (20, 18)] # координаты "суставов" пальцев, кроме большого
-thumb_Coord = (4, 2) # координаты "суставов" большого пальца 
+thumb_Coord = (4, 3) # координаты "суставов" большого пальца 
 
 while cap.isOpened():
     success, image = cap.read()
+    prevTime = time.time()
     if not success:
         print('Не удалось получить кадр с web-камеры')
         continue 
@@ -26,19 +28,32 @@ while cap.isOpened():
         upCount = 0 #счетчик пальцев 
         for handLms in multiLandMarks:
             mpDraw.draw_landmarks(image, handLms, mp_Hands.HAND_CONNECTIONS)
-            fingersList = [] # список пальцев
-            for idx, lm in enumerate(handLms.landmark):
+            fingersList = [] # список ключевых точек в пикселях 
+            for lm in handLms.landmark:
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)  
                 fingersList.append((cx, cy))
+            side = 'left'
+            if fingersList[5][0] > fingersList[17][0]:
+               side = 'right'
+
             for coordinate in finger_Coord:
                 if fingersList[coordinate[0]][1] < fingersList[coordinate[1]][1]:
                     upCount +=1
-            if fingersList[thumb_Coord[0]][0] < fingersList[thumb_Coord[1]][0]:
-                upCount += 1
+
+            if side == 'left':        
+                if fingersList[thumb_Coord[0]][0] < fingersList[thumb_Coord[1]][0]:
+                    upCount += 1
+            else:
+                if fingersList[thumb_Coord[0]][0] > fingersList[thumb_Coord[1]][0]:
+                    upCount += 1 
 
         cv2.putText(image, str(upCount), (50, 150), cv2.FONT_HERSHEY_PLAIN, 12, (0, 200, 50), 12)
-        print(upCount)            
+        print(upCount) 
+
+    currentTime = time.time()
+    fps = 1 / (currentTime - prevTime)
+    cv2.putText(image, f'FPS: {int(fps)}', (400, 150), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (240, 100, 0), 3)
     cv2.imshow('image', image)   
     if cv2.waitKey(1) & 0xFF == 27: # ESC
         break 
